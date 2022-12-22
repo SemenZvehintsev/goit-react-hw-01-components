63f8f319149547a67eada610aa6ee6a602e97aaa
SemenZvehintsev <kvadratniibomzh@gmail.com>
2022-12-22 13:19:04 +0200

    fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,10 +22,15 @@ export const App = () => {
       }}
     >
       <Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats}/>
+
       <Statistics title="Upload stats" stats={data} />
+
       <Statistics stats={data} />
+
       <FriendList friends={friends}/>
+      
       <TransactionHistory items={transactions}/>
+
     </div>
   );
 };
